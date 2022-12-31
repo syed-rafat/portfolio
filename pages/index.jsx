057edb52aa3a571fda2ba4d5478c1ce888e3b57d
rafat <hoqrafat@gmail.com>
2023-01-01 01:05:05 +0600

    Added about me, added styling, fixed card hover effect for projects

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,8 +6,6 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
-import CardList from "../components/portfolioCards/CardList";
-import Card from "../components/portfolioCards/Card";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +25,6 @@ function urlFor(source) {
 }
 
 export default function Home({ portfolio }) {
-  console.log(portfolio[0]);
   return (
     <div>
       <Head>
@@ -80,8 +77,25 @@ export default function Home({ portfolio }) {
             </div>
           </section>
           {/* About me section */}
-          <section className="h-[18rem]">
-            <h1>About Me</h1>
+          <section className="h-[36rem] m-12 px-6 py-12">
+            <h1 className="text-4xl text-accent pb-12 text-center">
+              About me
+            </h1>
+            <p className=" text-accent py-4 px-4 relative leading-relaxed w-[80%] m-auto shadow text-xl border-l-4 border-gradient_from">
+              Hi, my name is Syed Rafat and I am a full stack developer with
+              a strong understanding of modern web development technologies. I
+              have experience building complex web applications using Next.js
+              for server-side rendering and optimized performance, React.js for
+              building reusable UI components, and Tailwind for rapid
+              prototyping and styling. On the backend, I have used Django and
+              Django Restframework to build robust APIs and handle database
+              management. I have a strong understanding of RESTful API design
+              and have experience implementing authentication and authorization
+              using JSON Web Tokens. I have also used SQL and PostgreSQL for
+              database management. I have worked with linux system in the cloud and hosted on AWS and Azure.
+              I am currently looking for a full-time
+              remote position as a full stack developer.
+            </p>
           </section>
           {/* Tech stack section */}
 
@@ -90,37 +104,40 @@ export default function Home({ portfolio }) {
             <div></div>
           </section>
           {/* Project section */}
-          <section>
-            <h1>My Projects</h1>
-            <div className="px-4 py-12">
+          <section className="relative m-12">
+            <h1 className="text-center text-4xl text-accent">Projects</h1>
+            <div className="px-4 py-12 transition-all duration-300">
               <div className="flex flex-wrap -mx-2">
                 {portfolio.map((project) => (
-                  <div className="px-2 w-full sm:w-1/2 lg:w-1/3" key={project.title}>
                   <div
-                    className="max-w-sm rounded overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300"
+                    className="px-2 w-full sm:w-1/2 lg:w-1/3 group"
+                    key={project.title}
                   >
-                    <img
-                      className="w-full h-64 object-cover"
-                      src={urlFor(project.image).url()}
-                      alt={project.title}
-                    />
-                    <div className="px-6 py-4">
-                      <div className="font-bold text-xl mb-2">
-                        {project.title}
+                    <div className="max-w-sm rounded overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300">
+                      <img
+                        className="w-full h-64 object-cover"
+                        src={urlFor(project.image).url()}
+                        alt={project.title}
+                      />
+                      <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">
+                          {project.title}
+                        </div>
+                        {/* Hide description, only show when hovering over card */}
+
+                        <p className="text-gray-700 text-base hidden group-hover:block">
+                          {project.description}
+                        </p>
                       </div>
-                      <p className="text-gray-700 text-base">
-                        {project.description}
-                      </p>
+                      <div className="px-6 py-4">
+                        <Link
+                          href={project.link}
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                          View Project
+                        </Link>
+                      </div>
                     </div>
-                    <div className="px-6 py-4">
-                      <Link
-                        href={project.link}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                      >
-                        View Project
-                      </Link>
-                    </div>
-                  </div>
                   </div>
                 ))}
               </div>
