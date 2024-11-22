574560bf18f460e1fcbc4c5e8ef0cb620c87515e
syed-rafat <hoqrafat@gmail.com>
2024-11-22 13:01:38 +0600

    google analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LE26H557TB"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
