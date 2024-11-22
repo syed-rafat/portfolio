dc6866833cdc6cd0df57a36eb5b62b348aaebb51
syed-rafat <hoqrafat@gmail.com>
2024-11-22 12:37:04 +0600

    fixed contact form

--- components/contactForm.js
+++ components/contactForm.js
@@ -49,7 +49,28 @@ const ContactForm = () => {
       validationSchema={validationSchema}
       onSubmit={handleSubmit}
     >
-      {/* ... rest of your existing form JSX ... */}
+      {({ isSubmitting }) => (
+        <Form className='flex flex-col w-[45%] mx-auto pt-12 sm:w-[80%] max-w-[26rem] sm:ml-[10vw]'>
+          {formSuccess && <p>Your message was sent successfully!</p>}
+          <Toaster />
+            <h1 className='text-4xl text-center'>Contact</h1>
+            <label className='text-2xl text-text py-8'>Name</label>
+          <Field type="text" name="name" placeholder="Name" className="rounded p-2 text-mainbg w-full"/>
+          <ErrorMessage name="name" component="div" className='text-yellow-300 text-base'/>
+
+            <label className='text-2xl text-text py-8'>Email</label>
+          <Field type="email" name="email" placeholder="Email" className="rounded p-2 text-mainbg w-full"/>
+          <ErrorMessage name="email" component="div" className='text-yellow-300 text-base'/>
+
+            <label className='text-2xl py-8 text-text'>Message</label>
+          <Field as="textarea" name="message" placeholder="Message" className="rounded p-2 text-mainbg w-full"/>
+          <ErrorMessage name="message" component="div" className='text-yellow-300 text-base'/>
+
+          <button type="submit" disabled={isSubmitting} className="bg-gradient-to-r from-gradient_from to-gradient_to border-mainbg text-mainbg font-semibold  py-2 px-3 rounded-xl mt-12">
+            Send
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
